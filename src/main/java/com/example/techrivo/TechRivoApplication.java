0aa6a215b1b61fb87fea3505fbe7b0426c5f45a0
yusuf <yusuf@localhost>
2022-01-07 20:42:14 +0300

    initial commit

---
+++ src/main/java/com/example/techrivo/TechRivoApplication.java
@@ -0,0 +1,13 @@
+package com.example.techrivo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechRivoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TechRivoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/techrivo/TechRivoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.techrivo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechRivoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
